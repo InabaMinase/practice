d956c1c54c44f9f03e474a211776f50edd6e32c2
Minase <m.inaba.sys23@morijyobi.ac.jp>
2023-10-05 15:09:50 +0900

    HelloWorld
    １００回出力

--- pra2.java
+++ pra2.java
@@ -1,8 +1,11 @@
+
 public class UZ {
     public static void main(String[] args) {
+
+        for ( int i = 0; i <= 100; i++) {
             System.out.println("HelloWorld");
-         
+            System.out.println(i);
         }
     }
-
+}
 
